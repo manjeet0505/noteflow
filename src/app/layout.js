1c582fcb9e483761a3b86101746447705bb862fb
Manjeet Kumar Mishra <mishramanjeet26@gmail.com>
2025-07-31 01:33:39 +0530

    Add favicon support to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,13 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        {/* Uncomment below if you add PNG fallbacks in the future */}
+        {/* <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" /> */}
+        {/* <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" /> */}
+        {/* <link rel="apple-touch-icon" href="/favicon-32x32.png" /> */}
+      </head>
       <body className="min-h-screen bg-gray-50">
         <GoogleOAuthProvider clientId={googleClientId}>
           <AuthProvider>
